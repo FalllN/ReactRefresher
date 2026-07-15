e8d11b0a518a047c30c35ed199ba48333ed1858d
Dawson Gilbert <falln.beatz@gmail.com>
2026-07-14 23:45:22 -0400

    Add project files

---
+++ React Refresher/src/components/PostsList.jsx
@@ -0,0 +1,36 @@
+import Post from "./Post.jsx";
+import classes from './PostsList.module.css';
+import NewPost from "./NewPost.jsx";
+import {useState} from "react";
+import Modal from "./Modal.jsx";
+
+function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value);
+    }
+
+return(
+    <>
+        <Modal>
+            <NewPost
+                onBodyChange={bodyChangeHandler}
+                onAuthorChange={authorChangeHandler}/>
+
+        </Modal>
+
+    <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody}/>
+        <Post author="Jessica" body="React is awesome!"/>
+    </ul>
+        </>
+)
+}
+
+export default PostsList;
