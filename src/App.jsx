c287c17a30e1955ff1795144e2d49eb6263ec928
Dawson Gilbert <falln.beatz@gmail.com>
2026-07-20 08:20:17 -0400

    App filter uses search and filter status

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import ticketsData from './data/tickets';
 import Header from './components/Header';
 import TicketCard from './components/TicketCard';
 import StatusFilter from './components/StatusFilter';
+import SearchBar from './components/SearchBar';
 
 function App() {
   const [tickets] = useState(ticketsData);
   const [filterStatus, setFilterStatus] = useState('All');
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const filteredTickets =
-    filterStatus === 'All'
-      ? tickets
-      : tickets.filter((ticket) => ticket.status === filterStatus);
+  const filteredTickets = tickets.filter((ticket) => { //results get filtered regardless essentially cant do an if else
+      const checkStatus =
+          filterStatus === 'All' || ticket.status === filterStatus;
+
+      const checkSearch = ticket.customer.includes(searchTerm) || ticket.subject.includes(searchTerm);
+      return checkStatus && checkSearch;
+  });
 
   return (
     <main className="mx-auto max-w-3xl px-5 py-8">
       <Header />
-
+        <SearchBar value={searchTerm} onChange={setSearchTerm} />
       <StatusFilter currentFilter={filterStatus} onFilterChange={setFilterStatus} />
 
       <div className="grid gap-4">
@@ -24,7 +29,7 @@ function App() {
           filteredTickets.map((ticket) => <TicketCard key={ticket.id} ticket={ticket} />)
         ) : (
           <p className="py-10 text-center text-slate-500">
-            No tickets found with status "{filterStatus}".
+            No tickets found with status "{filterStatus}" and search term "{searchTerm}".
           </p>
         )}
       </div>
